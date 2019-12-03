377d1bb528cf97a5c16e02c275c6bb9fcd74a25a
Stefan Rakel <rakel@embedded.rwth-aachen.de>
2019-12-03 18:33:47 +0100

    Day3 Part3 solved with incorrect understanding of visiting a position multiple times

--- src/three.rs
+++ src/three.rs
@@ -1,109 +1,153 @@
+use std::collections::{HashMap, HashSet};
 
-#[derive(Debug)]
-struct Interval {
-    low: i64,
-    high: i64
+#[derive(Debug, Copy, Clone, Hash, Eq, PartialEq)]
+struct Point {
+    x: i64,
+    y: i64,
 }
+
+type Distance = i64;
 
 #[derive(Debug)]
 struct Line {
-    x: Interval,
-    y: Interval,
+    points: HashMap<Point, Distance>,
 }
 
-impl Interval {
-    fn new(a: i64, b: i64) -> Self {
-        Self {
-            low: a.min(b),
-            high: a.max(b),
-        }
+impl Point {
+    fn new(x: i64, y: i64) -> Self {
+        Self { x, y }
     }
 
-    fn intersect(&self, other: &Self) -> Option<Self> {
-        if self.high < other.low || self.low > other.high {
-            return None;
-        }
-        Some(Interval {
-            low: self.low.max(other.low),
-            high: self.high.min(other.high),
-        })
+    fn manhatten_dist(&self, other: &Self) -> i64 {
+        (self.x - other.x).abs() + (self.y - other.y).abs()
     }
 }
 
 impl Line {
-    fn new(input: &str) -> Vec<Self> {
+    fn new(input: &str) -> Self {
         let instructions: Vec<&str> = input.trim().split(",").collect();
 
-        let mut lines = Vec::new();
-        let mut start_point = (0, 0);
+        let mut point = Point::new(0, 0);
+        let mut distance = 0;
+
+        let mut line = Self {
+            points: HashMap::new(),
+        };
         for instruction in &instructions {
-            let (line, point) = Self::line_from_instruction(instruction, start_point);
-            start_point = point;
-            lines.push(line);
+            let (filled_point, filled_distance) =
+                line.fill_from_instruction(instruction, point, distance);
+            point = filled_point;
+            distance = filled_distance;
         }
-        lines
+        line
     }
 
-    fn line_from_instruction(instruction: &str, start: (i64, i64)) -> (Self, (i64, i64)) {
+    fn fill_from_instruction(
+        &mut self,
+        instruction: &str,
+        start: Point,
+        distance: i64,
+    ) -> (Point, Distance) {
         let code = &instruction[0..1];
         let length: i64 = instruction[1..].parse().unwrap();
-        let mut up = 0;
-        let mut right = 0;
+        let mut points = Vec::new();
+        let end;
         match code {
-            "U" => up = length,
-            "D" => up = -length,
-            "R" => right = length,
-            "L" => right = -length,
+            "U" => {
+                for y in start.y..=start.y + length {
+                    points.push(Point::new(start.x, y));
+                }
+                end = Point::new(start.x, start.y + length);
+            }
+            "D" => {
+                for y in (start.y - length..=start.y).rev() {
+                    points.push(Point::new(start.x, y));
+                }
+                end = Point::new(start.x, start.y - length);
+            }
+            "R" => {
+                for x in start.x..=start.x + length {
+                    points.push(Point::new(x, start.y));
+                }
+                end = Point::new(start.x + length, start.y);
+            }
+            "L" => {
+                for x in (start.x - length..=start.x).rev() {
+                    points.push(Point::new(x, start.y));
+                }
+                end = Point::new(start.x - length, start.y);
+            }
             _ => panic!("Invalid instruction"),
-        }
-        let line = Self {
-            x: Interval::new(start.0, start.0 + right),
-            y: Interval::new(start.1, start.1 + up),
         };
-        (line, (start.0 + right, start.1 + up))
+        let mut distance = distance;
+        for point in points {
+            distance = self.fill_point(point, distance);
+            distance += 1;
+        }
+
+        (end, distance)
     }
 
-    fn intersect(&self, other: &Self) -> Option<Vec<(i64, i64)>> {
-        if let Some(x_intersect) = self.x.intersect(&other.x) {
-            if let Some(y_intersect) = self.y.intersect(&other.y) {
-                let mut points = Vec::new();
-                for x in x_intersect.low..=x_intersect.high {
-                    for y in y_intersect.low..=y_intersect.high {
-                        points.push((x,y));
-                    }
-                }
-                return Some(points);
+    fn fill_point(&mut self, point: Point, distance: Distance) -> Distance {
+        //Because distance is monotoniously increasing we can always assume a distance already
+        //saved is lower than the one passed to this function
+        self.points.entry(point).or_insert(distance).clone()
+    }
+
+    fn intersections(&self, other: &Self) -> Vec<Point> {
+        let my_points: HashSet<Point> = self.points.keys().map(|x| x.clone()).collect();
+        let their_points: HashSet<Point> = other.points.keys().map(|x| x.clone()).collect();
+        my_points.intersection(&their_points).map(|x| x.clone()).collect()
+    }
+
+    fn distance(&self, point: &Point) -> Option<&Distance> {
+        self.points.get(point)
+    }
+
+    fn combined_distance(&self, other: &Self, point: &Point) -> Option<Distance> {
+        if let Some(my_distance) = self.distance(point) {
+            if let Some(other_distance) = other.distance(point) {
+                return Some(my_distance + other_distance);
             }
         }
         None
     }
 }
 
-
-fn manhattan_dist(point: &(i64, i64)) -> i64 {
-    return point.0.abs() + point.1.abs();
+fn print_points(line: &Line) {
+    let mut points = Vec::new();
+    for (key, val) in line.points.iter() {
+        points.push((key, val));
+    }
+    points.sort_by_key(|x| x.1);
+    for point in points {
+        println!("{:?}", point);
+    }
 }
 
 pub fn run(input: Vec<String>) {
     if input.len() != 2 {
         panic!("More than two lines!");
     }
-    let lines_1 = Line::new(&input[0]);
-    let lines_2 = Line::new(&input[1]);
-
-    let mut intersection_points: Vec<(i64, i64)> = Vec::new();
-    //Get intersections
-    for line_1 in &lines_1 {
-        for line_2 in &lines_2 {
-            if let Some(mut points) = line_1.intersect(line_2) {
-                intersection_points.append(&mut points);
-            }
-        }
-    }
-    //Remove (0,0) Intersection
-    intersection_points.retain(|&x| x != (0,0));
+    let line_1 = Line::new(&input[0]);
+    let line_2 = Line::new(&input[1]);
+    let mut intersections = line_1.intersections(&line_2);
+    //remove (0,0)
+    intersections.retain(|x| *x != Point::new(0,0));
+    
+    let closest_manhatten_intersection = intersections.iter().min_by_key(|x| x.manhatten_dist(&Point::new(0,0))).unwrap();
+    let closest_distance_intersection = intersections.iter().min_by_key(|x| line_1.combined_distance(&line_2, x).unwrap()).unwrap();
         
-    let &closest_intersection = intersection_points.iter().min_by(|x, y| manhattan_dist(x).cmp(&manhattan_dist(y))).unwrap();
-    println!("Day3 Part1: Closest intersection: ({},{}) Dist: {}", closest_intersection.0, closest_intersection.1, manhattan_dist(&closest_intersection));
+
+    print_points(&line_1);
+    println!("");
+    print_points(&line_2);
+
+    // for point in &intersections {
+    //     println!("{:?}", point);
+    // }
+
+    println!("Day3 Part1: ({},{}) Manhatten_Distance: {}, Distance: {}", closest_manhatten_intersection.x, closest_manhatten_intersection.y, closest_manhatten_intersection.manhatten_dist(&Point::new(0,0)), line_1.combined_distance(&line_2, closest_manhatten_intersection).unwrap());
+    println!("Day3 Part2: ({},{}) Manhatten_Distance: {}, Distance: {}", closest_distance_intersection.x, closest_distance_intersection.y, closest_distance_intersection.manhatten_dist(&Point::new(0,0)), line_1.combined_distance(&line_2, closest_distance_intersection).unwrap());
 
 }
